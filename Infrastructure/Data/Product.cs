e8fa2b712392ef8e7ea4ef4a6832801df4537bca
Hubert Le <namelem4u@gmail.com>
2026-01-04 20:15:13 +0700

    First commit

--- Infrastructure/Data/Product.cs
+++ Infrastructure/Data/Product.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
+﻿using System.ComponentModel.DataAnnotations;
 
-namespace ConsoleApp1.Data
+namespace ConsoleApp1.Infrastructure.Data
 {
     internal class Product
     {
--- Program.cs
+++ Program.cs
@@ -1,7 +1,4 @@
-﻿
-
-
-using ConsoleApp1.Data;
+﻿using ConsoleApp1.Infrastructure.Data;
 
 var dbContext = new ProductContext();
 
